1a5aa70bd43ac765ffee2f17481162857932fbfc
Kyle Seipp <kpseipp@gmail.com>
2020-11-02 04:09:37 -0800

    everything is nice and pretty now
    speculate that things are actually guaranteed to enter a loop? maybe thats based on dimensions?
    no it is forced bc we only have so many pixels and the dimensions should get stuck once
    we do some small number of finite steps at the beginning based on parity

--- squish.py
+++ squish.py
@@ -31,12 +31,14 @@ def random_pixel():
 stone1 = random_pixel()
 stone2 = random_pixel()
 stone3 = random_pixel()
-row1 = [stone1 for _ in range(5)]
-row2 = [stone2 for _ in range(5)]
-row3 = [stone3 for _ in range(5)]
+row1 = [stone1 for _ in range(9)]
+row2 = [stone2 for _ in range(9)]
+row3 = [stone3 for _ in range(9)]
+row4 = [stone3 for _ in range(9)]
 
 # make sure they are copies. might need to be more clever bc the pixels are still the same
-starting_rock = [row1[:], row2[:], row3[:]]
+starting_rock = [row1[:], row1[:], row2[:], row4[:], row2[:], row3[:], row3[:]]
+#starting_rock = [row1, row2, row3]
 
 # ok now we need to stretch
 
@@ -49,16 +51,17 @@ def stretch(rock):
     # This might cause us to lose some material?
     # What if we just did it from the top?
     # the bottom row might end up as itself twice over
-    print(rock)
+    #print(rock)
     out_rock = [[0 for _ in range(2*len(rock[0]))] for _ in range(div2(len(rock)))]
     for i, row in enumerate(rock):
         for j, pixel in enumerate(row):
             out_rock[i//2][2*j+i%2] = pixel
-            print(out_rock)
+            #print(out_rock)
     i = len(rock)
-    for j, pixel in enumerate(rock[-1]):
-        out_rock[i//2][2*j+i%2] = pixel
-        print(out_rock)
+    if i%2 == 1:
+        for j, pixel in enumerate(rock[-1]):
+            out_rock[i//2][2*j+i%2] = pixel
+            #print(out_rock)
     return out_rock
 
 def fold(rock):
@@ -83,3 +86,64 @@ def squish(r1, r2):
     out_rock = r2[:] + r1[:]
     return out_rock
     
+# now we need to make it so that we can actually visualize this
+from tkinter import *
+root = Tk()
+def rgb_to_color(r,g,b):
+    """ Takes three integers from 0-255 and transforms them into
+    a #XXYYZZ color string. """
+    color = "#"
+    # print r,g,b
+    # print hex(r),hex(g),hex(b)
+    r,g,b = int(r), int(g), int(b)
+    if len(hex(r)[2:]) != 2:
+        color += '0'
+    color += hex(r)[2:]
+    if len(hex(g)[2:]) != 2:
+        color += '0'
+    color += hex(g)[2:]
+    if len(hex(b)[2:]) != 2:
+        color += '0'
+    color += hex(b)[2:]
+    # print color
+    return color
+height = 200
+width = 400
+root.geometry(str(width) + 'x' + str(height))
+canvas = Canvas(root, width=width, height=height)
+canvas.pack()
+
+item_list = []
+def render(rock, scale=20, item_list=item_list, x_push=0):
+    if not(x_push):
+        for item in item_list:
+            canvas.delete(item)
+    for i,row in enumerate(rock):
+        for j, pixel in enumerate(row):
+            item = canvas.create_rectangle(scale*j + scale*x_push, scale*i, scale*j + scale + x_push*scale,scale*i + scale,
+                        fill=rgb_to_color(pixel.r, pixel.g, pixel.b))
+            item_list.append(item)
+    canvas.update()
+
+import time
+def metamorph(rock):
+    render(rock)
+    time.sleep(3)
+
+    stretch_rock = stretch(rock)
+    render(stretch_rock)
+    time.sleep(3)
+
+    l, r = fold(stretch_rock)
+    render(l)
+    render(r, x_push=len(l[0])+1)
+    time.sleep(2)
+
+    new_rock = squish(l, r)
+    render(new_rock)
+    return new_rock
+
+def inf_met():
+    r = starting_rock
+    while True:
+        r = metamorph(r)
