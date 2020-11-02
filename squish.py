82a0da778b0418818d4d3efc3f28d7f4e40d4668
Kyle Seipp <kpseipp@gmail.com>
2020-11-02 01:56:53 -0800

    beginning idea of the project

---
+++ squish.py
@@ -0,0 +1,11 @@
+# Ok so what is it that we want to do we want to take a set of strata like differnet colors of clay
+#and then stretch them out
+#then fold them over
+#and smush them again
+
+#1. we need to seam carve? wait do we?
+# maybe we can do the stretchin gfirst.
+#2. fold this part seems maybe difficult?
+#3. put a copy back on top. this seems legit.
+
+# So first
